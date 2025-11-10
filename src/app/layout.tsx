adc96cbcb3bef2cf2ae425ed7156c270a120995a
tottimilan <tottimilanmail@gmail.com>
2025-11-10 09:08:22 +0000

    feat: Implementar Google Tag Manager completo (GTM-P7ZVFB2C)
    - Reemplazar código gtag directo por GTM completo en layout.tsx
    - Añadir script GTM en head y noscript en body
    - Preparado para configuración de GA4, Google Ads y eventos personalizados
    - Verificado que build funciona correctamente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,17 +133,16 @@ export default function RootLayout({
     <html lang="es" dir="ltr">
       <head>
         {/* Google Tag Manager */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17559381440"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-17559381440');
-            `,
-          }}
-        />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P7ZVFB2C');
+          `,
+        }} />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -156,6 +155,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body className="font-sans antialiased" suppressHydrationWarning={true}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7ZVFB2C"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}>
+          </iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Global Website Schema for better SEO */}
         <WebsiteSchema />
         <LocalBusinessSchema />
